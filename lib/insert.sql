357d4cce0511afc17a339401c417a54276a2bd52
Juan Tenezaca <tenj34@gmail.com>
2018-09-15 23:04:42 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,3 +1,4 @@
 INSERT into bears (name) VALUES ('Mr. Chocolate')
 INSERT into bears (name) VALUES ('Rowdy')
 INSERT into bears (name) VALUES ('Tabitha')
+INSERT into bears (name) VALUES ('Sergeant Brown')
