575ed2fe417a0aa7e5150c8e60e045fbd5557a2b
Juan Tenezaca <tenj34@gmail.com>
2018-09-15 23:38:42 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -15,6 +15,8 @@ def selects_oldest_bear_and_returns_name_and_age
 end
 
 def select_youngest_bear_and_returns_name_and_age
+  "SELECT name, age FROM bears WHERE age = (SELECT MIN(age) FROM bears)"
+
 end
 
 def selects_most_prominent_color_and_returns_with_count
